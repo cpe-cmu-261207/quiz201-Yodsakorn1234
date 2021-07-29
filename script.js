466b746433ec3aa90fbf184643182b66a29057e5
Yodsakorn <menming1234@outlook.com>
2021-07-29 14:19:53 +0700

    script.js

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ const color = document.querySelector('#color')
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+  if(btn_toggle.innerHTML == "Show Calculation"){
+btn_toggle.innerHTML = "Author";
+}
+else btn_toggle.innerHTML = "Show Calculaion"
+}
+const wordCut = (str) => {
+  let arrText = [];
+  const word = str.split(" ");
+  arrText.push(word);
+  return arrText;
+}
+const textOverLength = wordCut(tempText);
+const textCutted = wordCut(tempText);
+for(let i=0; i < 320; i++){
 }
 
-// more codes for Search and Reset buttons here
